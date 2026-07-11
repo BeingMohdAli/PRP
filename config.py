f5f961513e0c07a59913c04fca4ab4029a5005a0
BeingMohdAli <mohdalisaad868@gmail.com>
2026-07-11 14:04:49 +0530

    PRP

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+
+
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
